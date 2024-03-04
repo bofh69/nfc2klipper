ec9d3c199c77dabdc2e9bd4059ae58ae8d306c85
Sebastian Andersson <sebastian@bittr.nu>
2024-03-04 19:25:18 +0100

    Add the beginning of a program to read RFID tags and call klipper

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # nfc2klipper
-Set loaded spool &amp; filament in klipper from NFC tags
+
+Set loaded spool &amp; filament in klipper from NFC tags.
+
+WARNING: This is Work In Progress and not yet tested
+
+
+## Preparing an NFC reader
+
+I use a PN532 based reader (Elechouse PN532 NFC RFID Module V3, if you
+want to use the same) connected via UART to the raspberry pi where this
+program is running.
+
+
+Many pages suggest connecting its VCC pin to 5V on the RPi. Don't!
+It can run from 3.3V and then it won't risk destroying the RPi's GPIO pins.
+
+
+See [here](https://learn.adafruit.com/adafruit-nfc-rfid-on-raspberry-pi/pi-serial-port)
+for how to configure a raspberry pi for it.
+
+
+## Preparing klipper
+
+Klipper should have two gcode macros:
+
+* SET_ACTIVE_FILAMENT ID=n
+* SET_ACTIVE_SPOOL ID=n
+
+
+I use this configuration:
+```ini
+[gcode_macro SET_ACTIVE_SPOOL]
+gcode:
+  {% if params.ID %}
+    {% set id = params.ID|int %}
+    {action_call_remote_method(
+       "spoolman_set_active_spool",
+       spool_id=id
+    )}
+  {% else %}
+    {action_respond_info("Parameter 'ID' is required")}
+  {% endif %}
+
+[gcode_macro SET_ACTIVE_FILAMENT]
+variable_active_filament: 0
+gcode:
+  {% if params.ID %}
+    {% set id = params.ID|int %}
+    SET_GCODE_VARIABLE MACRO=SET_ACTIVE_FILAMENT VARIABLE=active_filament VALUE={id}
+  {% else %}
+    {action_respond_info("Parameter 'ID' is required")}
+  {% endif %}
+
+[gcode_macro ASSERT_ACTIVE_FILAMENT]
+gcode:
+  {% if params.ID %}
+    {% set id = params.ID|int %}
+    {% current_id = printer["gcode_macro set_active_filament"].active_filament %}
+    {% if id != current_id %}
+      {# TODO: Change to PAUSE & M117 message #}
+      {action_raise_error("Wrong filament is loaded, should be " + id)}
+    {% endif %}
+  {% else %}
+    {action_respond_info("Parameter 'ID' is required")}
+  {% endif %}
+```
+
+## Preparing tags
+
+The tags should contain an NDEF record with a text block like this:
+```
+SPOOL: 3
+FILAMENT: 2
+```
+
+The numbers are the id numbers that will be sent to the macros in
+klipper via the [Moonraker](https://github.com/Arksine/moonraker) API.
+
+
+I've written to my tags with an Android phone and NXP's TagWriter.
+
+TODO: Write it with the NFC interface. A TUI selecting the spool to write?
---
+++ nfc2klipper.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+"""Program to set current filament & spool in klipper."""
+
+import argparse
+
+import nfc
+import requests
+
+SPOOL = "SPOOL"
+FILAMENT = "FILAMENT"
+NDEF_TEXT_TYPE = "urn:nfc:wkt:T"
+
+parser = argparse.ArgumentParser()
+# description="Fetches filaments from Spoolman and creates SuperSlicer filament configs.",
+
+parser.add_argument("--version", action="version", version="%(prog)s 0.0.1")
+
+parser.add_argument(
+    "-c",
+    "--clear",
+    action="store_true",
+    help="Clears the spool & filamnet when no tag is present",
+)
+
+parser.add_argument(
+    "-d",
+    "--nfc-device",
+    metavar="device",
+    default="ttyS0",
+    help="Which NFC reader to use, see "
+    + "https://nfcpy.readthedocs.io/en/latest/topics/get-started.html#open-a-local-device"
+    + " for format",
+)
+
+parser.add_argument(
+    "-u",
+    "--url",
+    metavar="URL",
+    default="http://mainsailos.local",
+    help="URL for the moonraker installation",
+)
+
+
+def set_spool_and_filament(url: str, spool: int, filament: int):
+    """Calls moonraker with the current spool & filament"""
+
+    commands = {
+        "commands": [
+            f"SET_ACTIVE_SPOOL ID={spool}",
+            f"SET_ACTIVE_FILAMENT ID={filament}",
+        ]
+    }
+
+    response = requests.post(
+        url + "/api/printer/command", timeout=10, json=commands
+    )
+    if response.status_code != 200:
+        raise ValueError(f"Request to moonraker failed: {response}")
+
+
+def get_data_from_ndef_records(records):
+    """Find wanted data from the NDEF records.
+
+    >>> import ndef
+    >>> record0 = ndef.TextRecord("")
+    >>> record1 = ndef.TextRecord("SPOOL:23\\n")
+    >>> record2 = ndef.TextRecord("FILAMENT:14\\n")
+    >>> record3 = ndef.TextRecord("SPOOL:23\\nFILAMENT:14\\n")
+    >>> get_data_from_ndef_records([record0])
+    (None, None)
+    >>> get_data_from_ndef_records([record3])
+    ('23', '14')
+    >>> get_data_from_ndef_records([record1])
+    ('23', None)
+    >>> get_data_from_ndef_records([record2])
+    (None, '14')
+    >>> get_data_from_ndef_records([record0, record3])
+    ('23', '14')
+    >>> get_data_from_ndef_records([record3, record0])
+    ('23', '14')
+    >>> get_data_from_ndef_records([record1, record2])
+    ('23', '14')
+    >>> get_data_from_ndef_records([record2, record1])
+    ('23', '14')
+    """
+
+    spool = None
+    filament = None
+
+    for record in records:
+        if record.type == NDEF_TEXT_TYPE:
+            for line in record.text.splitlines():
+                line = line.split(":")
+                if len(line) == 2:
+                    if line[0] == SPOOL:
+                        spool = line[1]
+                    if line[0] == FILAMENT:
+                        filament = line[1]
+        else:
+            print(f"Read other record: {record}")
+
+    return spool, filament
+
+
+def on_nfc_connect(tag):
+    """Handles a read tag"""
+
+    if tag.ndef is None:
+        print("The tag doesn't have NDEF records")
+        return True
+
+    spool, filament = get_data_from_ndef_records(tag.ndef.records)
+
+    if not args.clear:
+        if not (spool and filament):
+            print("Did not find spool and filament records in tag")
+    if args.clear or (spool and filament):
+        if not spool:
+            spool = 0
+        if not filament:
+            filament = 0
+        set_spool_and_filament(args.url, spool, filament)
+
+    # Don't let connect return until the tag is removed:
+    return True
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    # Open NFC reader. Will throw an exception if it fails.
+    clf = nfc.ContactlessFrontend(args.nfc_device)
+
+    if args.clear:
+        # Start by unsetting current spool & filament:
+        set_spool_and_filament(args.url, 0, 0)
+
+    while True:
+        clf.connect(rdwr={"on-connect": on_nfc_connect})
+        # No tag connected anymore.
+        if args.clear:
+            set_spool_and_filament(args.url, 0, 0)
