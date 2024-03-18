0e1020af6b4b490fd0ae0a547715ffa2d24e712e
Sebastian Andersson <sebastian@bittr.nu>
2024-03-18 15:00:13 +0100

    WIP

--- nfc2klipper.py
+++ nfc2klipper.py
@@ -6,18 +6,24 @@
 """Program to set current filament & spool in klipper."""
 
 import argparse
+import asyncio
+import json
 
 import nfc
-import requests
+
+# import requests
 
 SPOOL = "SPOOL"
 FILAMENT = "FILAMENT"
 NDEF_TEXT_TYPE = "urn:nfc:wkt:T"
+VERSION = "0.0.2"
 
 parser = argparse.ArgumentParser()
 # description="Fetches filaments from Spoolman and creates SuperSlicer filament configs.",
 
-parser.add_argument("--version", action="version", version="%(prog)s 0.0.1")
+parser.add_argument(
+    "--version", action="version", version="%(prog)s " + VERSION
+)
 
 parser.add_argument(
     "-c",
@@ -36,16 +42,51 @@ parser.add_argument(
     + " for format",
 )
 
+# parser.add_argument(
+#    "-u",
+#    "--url",
+#    metavar="URL",
+#    default="http://mainsailos.local",
+#    help="URL for the moonraker installation",
+# )
+
 parser.add_argument(
-    "-u",
-    "--url",
-    metavar="URL",
-    default="http://mainsailos.local",
-    help="URL for the moonraker installation",
+    "-k",
+    "--klipper-uds",
+    metavar="PATH",
+    default="/tmp/klippy_uds",
+    help="Path to Klipper's API socket.",
 )
 
 
-def set_spool_and_filament(url: str, spool: int, filament: int):
+class KlipperConnection:
+    """Connection to klipper's API"""
+
+    def __init__(self):
+        self.reader = None
+        self.writer = None
+        self.msg_id = 1
+
+    async def connect(self, path):
+        """Connects to the Unix Domain Socket at _path_"""
+        (self.reader, self.writer) = await asyncio.open_unix_connection(path)
+
+    async def send(self, method, params):
+        """Sends a request to klipper"""
+        cmd = {"id": self.msg_id, "method": method, "params": params}
+        self.msg_id += 1
+        string = json.dumps(cmd, separators=(",", ":"))
+        self.writer.write(string.encode() + "\003")
+        await self.writer.drain()
+
+    async def send_gcode(self, gcode):
+        """Sends a gcode script to klipper"""
+        await self.send("gcode/script", {"script": gcode})
+
+
+async def set_spool_and_filament(
+    klipper: KlipperConnection, spool: int, filament: int
+):
     """Calls moonraker with the current spool & filament"""
 
     if "old_spool" not in set_spool_and_filament.__dict__:
@@ -59,28 +100,18 @@ def set_spool_and_filament(url: str, spool: int, filament: int):
         print("Read same spool & filament", flush=True)
         return
 
-    print(f"Sending spool #{spool}, filament #{filament} to klipper", flush=True)
-
-    commands = {
-        "commands": [
-            f"SET_ACTIVE_SPOOL ID={spool}",
-            f"SET_ACTIVE_FILAMENT ID={filament}",
-        ]
-    }
+    print(
+        f"Sending spool #{spool}, filament #{filament} to klipper", flush=True
+    )
 
     # In case the post fails, we might not know if the server has received
     # it or not, so set them to None:
     set_spool_and_filament.old_spool = None
     set_spool_and_filament.old_filament = None
 
-    try:
-        response = requests.post(
-            url + "/api/printer/command", timeout=10, json=commands
-        )
-        if response.status_code != 200:
-            raise ValueError(f"Request to moonraker failed: {response}")
-    except Exception as ex:  # pylint: disable=W0718
-        print(ex)
+    await klipper.send_gcode(
+        f"BEEP\nSET_ACTIVE_SPOOL ID={spool}\nSET_ACTIVE_FILAMENT ID={filament}\nBEEP"
+    )
 
     set_spool_and_filament.old_spool = spool
     set_spool_and_filament.old_filament = filament
@@ -130,41 +161,67 @@ def get_data_from_ndef_records(records):
     return spool, filament
 
 
-def on_nfc_connect(tag):
-    """Handles a read tag"""
+class Main:
+    """The main application"""
 
-    if tag.ndef is None:
-        print("The tag doesn't have NDEF records", flush=True)
+    def __init__(self, event_loop, args):
+        self.event_loop = event_loop
+        self.args = args
+        self.klipper = KlipperConnection()
+
+    def on_nfc_connect(self, tag):
+        """Handles a read tag"""
+
+        if tag.ndef is None:
+            print("The tag doesn't have NDEF records", flush=True)
+            return True
+
+        spool, filament = get_data_from_ndef_records(tag.ndef.records)
+
+        if not self.args.clear:
+            if not (spool and filament):
+                print(
+                    "Did not find spool and filament records in tag",
+                    flush=True,
+                )
+        if self.args.clear or (spool and filament):
+            if not spool:
+                spool = 0
+            if not filament:
+                filament = 0
+            future = asyncio.run_coroutine_threadsafe(
+                set_spool_and_filament(self.klipper, spool, filament),
+                self.event_loop,
+            )
+            # Wait for completion:
+            future.result()
+
+        # Don't let connect return until the tag is removed:
         return True
 
-    spool, filament = get_data_from_ndef_records(tag.ndef.records)
+    def _nfc_connect(self, clf):
+        clf.connect(rdwr={"on-connect": self.on_nfc_connect})
 
-    if not args.clear:
-        if not (spool and filament):
-            print("Did not find spool and filament records in tag", flush=True)
-    if args.clear or (spool and filament):
-        if not spool:
-            spool = 0
-        if not filament:
-            filament = 0
-        set_spool_and_filament(args.url, spool, filament)
+    async def run(self):
+        """The main async coroutine"""
+        await self.klipper.connect(self.args.klipper_uds)
 
-    # Don't let connect return until the tag is removed:
-    return True
+        # Open NFC reader. Will throw an exception if it fails.
+        clf = nfc.ContactlessFrontend(self.args.nfc_device)
+
+        if self.args.clear:
+            # Start by unsetting current spool & filament:
+            set_spool_and_filament(self.klipper, 0, 0)
+
+        while True:
+            await asyncio.to_thread(self._nfc_connect, clf)
+
+            # No tag connected anymore.
+            if self.args.clear:
+                set_spool_and_filament(self.klipper, 0, 0)
 
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-
-    # Open NFC reader. Will throw an exception if it fails.
-    clf = nfc.ContactlessFrontend(args.nfc_device)
-
-    if args.clear:
-        # Start by unsetting current spool & filament:
-        set_spool_and_filament(args.url, 0, 0)
-
-    while True:
-        clf.connect(rdwr={"on-connect": on_nfc_connect})
-        # No tag connected anymore.
-        if args.clear:
-            set_spool_and_filament(args.url, 0, 0)
+    loop = asyncio.get_event_loop()
+    main = Main(loop, parser.parse_args())
+    asyncio.run(main.run())
